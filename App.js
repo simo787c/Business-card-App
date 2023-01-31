950e11d9ebd2c7b003aa54561ab87986b4ece9b7
simo787c <92300326+simo787c@users.noreply.github.com>
2023-01-31 11:46:43 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Linking } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  logo: {
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderRadius: 100,
+    borderColor: "rgb(0,255,0)"
+  },
+});
+
+function handlePress() {
+  Linking.openURL('https://www.monsterenergy.com/dk/da/about-us').catch(err => console.error('An error occurred', err));
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Image source={ require("./media/logo.png")}
+          style={styles.logo} />
+
+      <Text style={{color:'white',fontFamily: "Franklin Gothic Medium",fontSize:30,margin:50}}>A LIFESTYLE IN A CAN</Text>
+      <Button title="About us" onPress={handlePress}></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
